75f60ef715dc75d1bf6645cd30a1aeddcb807531
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-06-30 10:50:52 +0200

    Modify setup.py for Python 3 with testing.
    Switch to pytest with tox for running tests.
    Split install_requires and tests_require to be module level variables.
    
    Add software classifiers, for niceness.

--- setup.py
+++ setup.py
@@ -17,7 +17,40 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+install_requires = (
+    'msgpack-python>=0.4.0,<1.0.0a0',
+    'six>=1.9.0,<2.0.0a0',
+)
+
+tests_require = (
+    'pytest-cov',
+    'pytest-cache',
+    'tox',
+)
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            'test',
+            '-v',
+            '--cov=crtauth',
+            '--cov-report=xml',
+            '--cov-report=term-missing',
+            '--result-log=pytest-results.log'
+        ]
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='crtauth',
@@ -27,5 +60,17 @@ setup(
     author_email='noa@spotify.com',
     license='Apache-2.0',
     packages=['crtauth'],
-    install_requires=['msgpack-python>=0.4.0']
+    install_requires=install_requires,
+    tests_require=tests_require,
+    cmdclass={
+        'test': PyTest,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ]
 )
